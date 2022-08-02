c74b8f37d2e0715c2d74ec51ea953f7c0008e32a
sangmin <110011732+samgmin@users.noreply.github.com>
2022-08-02 14:23:15 +0900

    Update BOJ_1100_정상민.java

--- Bronze/1100/BOJ_1100_정상민.java
+++ Bronze/1100/BOJ_1100_정상민.java
@@ -11,18 +11,18 @@ public class Main {
 		int flag = 0;
 		int cnt = 0;
 		for(int i=0;i<8;i++) {
-			chess[i] = br.readLine();
+			chess[i] = br.readLine(); // 체스판 저장
 		}
 		for(int i=0;i<8;i++) {
 			for(int j=0;j<8;j++) {
-				if(flag%2 == 0) {
-					if(chess[i].charAt(j++) == 'F') cnt++;
+				if(flag%2 == 0) { // 짝수 줄 일때
+					if(chess[i].charAt(j++) == 'F') cnt++; // 확인하고 더하기
 				}
-				else {
-					if(chess[i].charAt(++j) == 'F') cnt++; 
+				else { // 홀수 줄 일때
+					if(chess[i].charAt(++j) == 'F') cnt++; //확인하기 전에 더하기
 				}
 			}
-			flag++;
+			flag++; // 한줄 바뀔때마다
 		}
 		System.out.println(cnt);
 	}
