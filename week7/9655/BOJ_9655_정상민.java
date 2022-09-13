75e22e9eb72cbdeeb4aec9bcdca4b86019cd5eb7
sangmin <110011732+samgmin@users.noreply.github.com>
2022-09-13 20:34:16 +0900

    [ADD/Pass] BOJ 9655

---
+++ week7/9655/BOJ_9655_정상민.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		String result = N % 2 == 0 ? "CY" : "SK";
+		System.out.println(result);
+		
+	}
+
+}
