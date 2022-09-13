192b1f531b496312f7ac2111cea9cf26bf829adf
김아영 (KIM A YOUNG) <74530474+joen00@users.noreply.github.com>
2022-09-13 21:29:57 +0900

    Update BOJ_2310_김아영.java

--- week7/2310/BOJ_2310_김아영.java
+++ week7/2310/BOJ_2310_김아영.java
@@ -1,5 +1,3 @@
-package week7;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -10,7 +8,7 @@ import java.util.Queue;
 import java.util.Scanner;
 import java.util.StringTokenizer;
 
-public class BOJ_2310_김아영 {
+public class Main {
 
 	static int N, result;
 	static ArrayList<Integer>[] list;
@@ -18,50 +16,55 @@ public class BOJ_2310_김아영 {
 	static List<Point> info;
 
 	static class Point {
+		int idx;
 		char name;
 		int x;
+		int cnt;
 
-		public Point(char name, int x) {
+		public Point(int idx, char name, int x, int cnt) {
+			this.idx = idx;
 			this.name = name;
 			this.x = x;
+			this.cnt = cnt;
 		}
 	}
 
-	static void dfs(int cur, int cnt) {
+	static void bfs() {
 
-		if (result == 1 || cur > N)
-			return;
-		if (cur == N) {
-			result = 1;
-			return;
-		}
-
-		for (int idx : list[cur]) {
-			if (visit[idx])
-				continue;
-			if (info.get(idx).name == 'E') {
-				visit[idx] = true;
-				dfs(idx, cnt);
-				visit[idx] = false;
-			} else if (info.get(idx).name == 'L') {
-				// 만약에 내 현재 금액보다 L이 줄 수 있는 금액이 더 크면 갱신
-				if (info.get(idx).x > cnt) {
-					visit[idx] = true;
-					dfs(idx, info.get(idx).x);
-					visit[idx] = false;
-				} else {
-					visit[idx] = true;
-					dfs(idx, cnt);
-					visit[idx] = false;
+		Queue<Point> q = new LinkedList<>();
+		if (info.get(1).name == 'T' && info.get(1).x > 0) return;
+		q.offer(new Point(1, info.get(1).name, info.get(1).x, info.get(1).cnt));
+		visit[1] = true;
+		result = 0;
+		while (!q.isEmpty()) {
+			int size = q.size();
+			for(int i = 0; i < size; i++) {
+				Point cur = q.poll();
+				visit[cur.idx] = true;
+				if (cur.idx == N) {result = 1; return;}
+				// 내가 갈 수 있는 곳들을 list에서 방문하기
+				for(int idx : list[cur.idx]) {
+					if (visit[idx])continue;
+					if (info.get(idx).name == 'E') {
+						q.add(new Point(idx, info.get(idx).name, info.get(idx).x, cur.cnt));
+					}
+					else if (info.get(idx).name == 'L') {
+						// 만약에 내 현재 금액보다 L이 줄 수 있는 금액이 더 크면 갱신
+						if (info.get(idx).x > cur.cnt) {
+							q.add(new Point(idx, info.get(idx).name, info.get(idx).x, info.get(idx).x));
+						}
+						else {
+							q.add(new Point(idx, info.get(idx).name, info.get(idx).x, cur.cnt));
+						}
+					}
+					else if (info.get(idx).name == 'T') {
+						// 뺏을 수 있는 경우만 넣어주기
+						if (info.get(idx).x <= cur.cnt) {
+							q.add(new Point(idx, info.get(idx).name, info.get(idx).x, cur.cnt - info.get(idx).x));
+						}
+					}
 				}
-			} else if (info.get(idx).name == 'T') {
-				// 뺏을 수 있는 경우만 넣어주기
-				if (info.get(idx).x <= cnt) {
-					visit[idx] = true;
-					dfs(idx, cnt - info.get(idx).x);
-					visit[idx] = false;
-				} else
-					return;
+				// System.out.println(cur.idx);
 			}
 		}
 
@@ -82,32 +85,27 @@ public class BOJ_2310_김아영 {
 			for (int i = 0; i <= N; i++) {
 				list[i] = new ArrayList();
 			}
-			info.add(new Point('E', 0));
-			for (int i = 1; i <= N; i++) {
+			info.add(new Point(0, 'E', 0, 0));
+			for (int i = 0; i < N; i++) {
 				char a = sc.next().charAt(0);
 				int b = sc.nextInt();
-				info.add(new Point(a, b));
+				info.add(new Point(i + 1, a, b, 0));
 				while (true) {
 					int to = sc.nextInt();
 					if (to == 0)
 						break;
-					list[i].add(to);
+					list[i + 1].add(to);
 				}
 			}
 			// 입력 end
 			// 만약에 0 이 트롤이고 소지금이 0이 아닌 경우 바로 no하기
 			// 아니면 bfs
-			if (info.get(1).name == 'T' && info.get(1).x > 0) {
-				System.out.println("No");
-				return;
-			}
-			result = 0;
-			visit[1] = true;
-			dfs(1, info.get(1).x);
-
+			bfs();
+			
 			if (result == 0) {
 				System.out.println("No");
-			} else {
+			}
+			else {
 				System.out.println("Yes");
 			}
 
