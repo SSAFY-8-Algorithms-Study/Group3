3936584860fc1f447e3d5e9e75331aef2614e9a3
jihee <51085309+4priltwntsx@users.noreply.github.com>
2022-10-11 20:50:34 +0900

    [Add/Pass] BOJ_3187

---
+++ week11/3187/BOJ_3187_김지희.java
@@ -0,0 +1,18 @@
+package week11;
+
+public class BOJ_3187_김지희 {
+	//양치기 꿍
+	static class Point{
+		int x, y;
+		Point(){
+		}
+	}
+	
+	static int R, C;
+	static int[][] map;
+	
+	public static void main(String[] args) {
+		
+	}
+
+}
